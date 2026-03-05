6aeda1d05b21d22023f77f61a3206eabeadd853f
Bibek <166113859+Bibek-M@users.noreply.github.com>
2026-03-05 22:41:01 +0530

    Add files via upload

---
+++ booksdb.js
@@ -0,0 +1,21 @@
+const books = {
+  "9781593279509": {
+    title: "Eloquent JavaScript",
+    author: "Marijn Haverbeke",
+    reviews: {}
+  },
+
+  "9780131103627": {
+    title: "The C Programming Language",
+    author: "Dennis Ritchie",
+    reviews: {}
+  },
+
+  "9780201633610": {
+    title: "Design Patterns",
+    author: "Erich Gamma",
+    reviews: {}
+  }
+};
+
+module.exports = books;
